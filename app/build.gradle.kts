f49de31b22fd08db99f27b8ee80bad8426326b78
Mandar Limaye <mandarl@fb.com>
2023-12-28 11:54:51 -0600

    Update compose lib

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     minSdk = 33
     targetSdk = 34
     versionCode = 1
-    versionName = "1.0"
+    versionName = "1.1"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     vectorDrawables {
@@ -50,7 +50,7 @@ dependencies {
 
   implementation("androidx.core:core-ktx:1.12.0")
   implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-  implementation("androidx.activity:activity-compose:1.8.1")
+  implementation("androidx.activity:activity-compose:1.8.2")
   implementation(platform("androidx.compose:compose-bom:2023.03.00"))
   implementation("androidx.compose.ui:ui")
   implementation("androidx.compose.ui:ui-graphics")
